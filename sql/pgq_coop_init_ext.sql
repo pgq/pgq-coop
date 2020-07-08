937ac2c8ab0eef414cfc21ef178c4ec36d0f7e50
Marko Kreen <markokr@gmail.com>
2020-07-09 00:05:31 +0300

    Sync test with pg13

--- sql/pgq_coop_init_ext.sql
+++ sql/pgq_coop_init_ext.sql
@@ -2,12 +2,5 @@
 
 create extension pgq;
 
-\set ECHO none
-\i structure/install.sql
-\set ECHO all
-
-create extension pgq_coop from 'unpackaged';
-drop extension pgq_coop;
-
 create extension pgq_coop;
 
